39deb2a9ce2eff62aa671d8178bbaf1c7d8c5206
Joaquin <joaquin.garcia.jimenez@gmail.com>
2022-03-22 14:19:27 +0100

    estructura inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { ListadoComponent } from './listado/listado.component';
+import { BotoneraComponent } from './botonera/botonera.component';
+import { DatosComponent } from './datos/datos.component';
+import { MapaComponent } from './mapa/mapa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    ListadoComponent,
+    BotoneraComponent,
+    DatosComponent,
+    MapaComponent
   ],
   imports: [
     BrowserModule,
